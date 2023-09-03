973b8b3872103371d74b9ea208235f177b5f5a8e
Semjon Murashov <semjon.murashov@gmail.com>
2023-09-03 17:40:23 +0300

    poslednee

--- src/If.java
+++ src/If.java
@@ -1,7 +1,12 @@
 public class If {
     public static void main(String[] args) {
-    if(5>4){
+        int myInt = 15;
+        if(myInt<10){
         System.out.println("da verno.");
-    }
+    } else if (myInt>20){
+        System.out.println("net ne verno");
+    }else {
+            System.out.println("ni odin iz pred slu4aev");
+        }
     }
 }
