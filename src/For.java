9b814e6eb5550caac9890554e7bcdfdd667b5791
Semjon Murashov <semjon.murashov@gmail.com>
2023-09-03 17:23:46 +0300

    added for file

---
+++ src/For.java
@@ -0,0 +1,9 @@
+public class For {
+    public static void main (String[] args){
+        for(int i = 0; i<10; i++){
+            for( int k = 0; k<10; k++){
+                System.out.println((i+1) * (k+1));
+            }
+        }
+    }
+ }
--- src/If.java
+++ src/If.java
@@ -1,5 +1,7 @@
 public class If {
     public static void main(String[] args) {
-
+    if(5>4){
+        System.out.println("da verno.");
+    }
     }
 }
