dce235032d41d479f43fc01ca0c447ed12bb79f5
Semjon Murashov <semjon.murashov@gmail.com>
2023-09-03 17:28:24 +0300

    v stolbik

--- src/For.java
+++ src/For.java
@@ -2,8 +2,9 @@ public class For {
     public static void main (String[] args){
         for(int i = 0; i<10; i++){
             for( int k = 0; k<10; k++){
-                System.out.println((i+1) * (k+1));
+                System.out.printf("%5d",(i+1) * (k+1));
             }
+            System.out.println();
         }
     }
  }
