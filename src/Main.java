474b7ba33f9c2ff1ff624fd89c915468e68a6f35
Semjon Murashov <semjon.murashov@gmail.com>
2023-09-03 15:27:07 +0300

    urok po peremennqm

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,14 @@ public class Main {
         System.out.println("World!");
         System.out.println("\"Hello!\"\nMen");
         /* Да уж... начало положено... будет тяжко!!! */
+
+
+        int age;
+        // int age=35
+        age = 35;
+        System.out.println("Vozrast: " + age + ".");
+        age = 30;
+        System.out.println(age);
     }
 
 }
