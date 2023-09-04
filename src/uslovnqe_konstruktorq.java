6de0dfdace666cc7607d3e095c24a134aa680038
Semjon Murashov <semjon.murashov@gmail.com>
2023-09-04 22:04:07 +0300

    last added

---
+++ src/uslovnqe_konstruktorq.java
@@ -0,0 +1,18 @@
+public class uslovnqe_konstruktorq {
+
+        public static void main(String[]args) {
+            //uslovnqe konstrukcii.
+            int a = 15, b = 10;
+            char sym1 = 'A', sym2 = 'A';
+            boolean isHasCar = false;
+            if (!isHasCar) {
+                System.out.println("da verno");
+                } else if (a>b) {
+                System.out.println("da o4enj verno");
+                }
+                else {
+                System.out.println("net ne verno");
+                }
+
+    }
+}
