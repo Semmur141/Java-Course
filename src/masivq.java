18555e27e42260d91caf2c49b6143179b3600067
Semjon Murashov <semjon.murashov@gmail.com>
2023-09-11 15:51:17 +0300

    massivq

---
+++ src/masivq.java
@@ -0,0 +1,13 @@
+public class masivq {
+    public static void main(String[] args) {
+        //массивы данных
+        int[] nums = new int[5];
+        nums[0] = 45;
+        nums[1] = 32;
+        nums[2] = 1;
+        nums[3] = 9;
+        nums[4] = 3;
+        int res = nums[2] + nums[3];
+        System.out.println(res);
+    }
+}
