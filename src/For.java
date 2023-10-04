df9fdd42cca54a89659b541e483bd9a8b013195d
Semjon Murashov <semjon.murashov@gmail.com>
2023-10-04 14:55:20 +0300

    last

--- src/For.java
+++ src/For.java
@@ -2,7 +2,7 @@ public class For {
     public static void main (String[] args){
         for(int i = 0; i<10; i++){
             for( int k = 0; k<10; k++){
-                System.out.printf("%5d",(i+1) * (k+1));
+                System.out.printf("%5d", (i+1) * (k+1));
             }
             System.out.println();
         }
--- src/collections.java
+++ src/collections.java
@@ -7,6 +7,7 @@ public class collections {
         ArrayList<Integer> numbers = new ArrayList<>();
         numbers.add(5);
         numbers.add(50);
+        numbers.add(1, 30);
 
         for(Integer el : numbers) {
             System.out.println(el);
