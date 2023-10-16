3df8b801f7c259dd55228dc707e13bae47174da1
Semjon Murashov <semjon.murashov@gmail.com>
2023-10-16 11:42:10 +0300

    last

---
+++ src/Constructions.java
@@ -0,0 +1,13 @@
+public class Constructions {
+    public static void main(String[] args) {
+        Human human1 = new Human();
+        human1.setName("Bob");
+        human1.setAge(40);
+    }
+}
+
+class Human {
+    private String name;
+    private  int age;
+
+}
