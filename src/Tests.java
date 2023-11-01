bc559a207e5286046a8467a4cf838bd549ec8347
Semjon Murashov <semjon.murashov@gmail.com>
2023-11-01 12:37:21 +0200

    last

--- src/Tests.java
+++ src/Tests.java
@@ -36,19 +36,63 @@ import java.util.Scanner;
 //}
 
 
+//public class Tests {
+//    public static void main(String[] args) {
+//        Scanner scan = new Scanner(System.in);
+//        System.out.println("Vvedite vremja: ");
+//        float time1 = scan.nextFloat();
+//        if ( time1 < 12 && time1 >= 6 ) {
+//            System.out.println("Dobroe utro!");
+//        }else if (time1 <= 18 && time1 >= 12 ){
+//            System.out.println("Dobrogo dnja!");
+//        }else if (time1 <= 6 && time1 >= 0 ){
+//            System.out.println("Dobroi no4i!");
+//        }else {
+//            System.out.println("Dobrogo ve4era!");
+//        }
+//    }
+//}
+
+
+
+//Примитивный калькулятор
+
 public class Tests {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Vvedite vremja: ");
-        float time1 = scan.nextFloat();
-        if ( time1 < 12 && time1 >= 6 ) {
-            System.out.println("Dobroe utro!");
-        }else if (time1 <= 18 && time1 >= 12 ){
-            System.out.println("Dobrogo dnja!");
-        }else if (time1 <= 6 && time1 >= 0 ){
-            System.out.println("Dobroi no4i!");
-        }else {
-            System.out.println("Dobrogo ve4era!");
+        Scanner scnan = new Scanner(System.in);
+        System.out.println("Введите первое число: ");
+        int num1 = scnan.nextInt();
+        System.out.println("Введите второе число: ");
+        int num2 = scnan.nextInt();
+        int res;
+        System.out.println("Введите действие: ");
+        String deistvie = scnan.nextLine();
+        deistvie = scnan.nextLine();
+
+        switch (deistvie){
+            case "+":
+                res = num1 + num2;
+                System.out.println("Результат: " + res);
+                break;
+            case "*":
+                res = num1 * num2;
+                System.out.println("Результат: " + res);
+                break;
+            case "/":
+                if (num2 == 0)
+                    System.out.println("На ноль делить нельзя!");
+                else{
+                res = num1 / num2;
+                System.out.println("Результат: " + res);
+                }
+                break;
+            case "-":
+                res = num1 - num2;
+                System.out.println("Результат: " + res);
+
+                break;
+            default:
+                System.out.println("Вы ввели что-то не то!");
         }
     }
 }
