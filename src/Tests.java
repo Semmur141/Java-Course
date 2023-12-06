faf4bff8e649e0e18d73407c4993a35e03679d65
Semjon Murashov <semjon.murashov@gmail.com>
2023-12-06 09:45:53 +0200

    last

--- src/Tests.java
+++ src/Tests.java
@@ -57,42 +57,208 @@ import java.util.Scanner;
 
 //Примитивный калькулятор
 
+//public class Tests {
+//    public static void main(String[] args) {
+//        Scanner scnan = new Scanner(System.in);
+//        System.out.println("Введите первое число: ");
+//        int num1 = scnan.nextInt();
+//        System.out.println("Введите второе число: ");
+//        int num2 = scnan.nextInt();
+//        int res;
+//        System.out.println("Введите действие: ");
+//        String deistvie = scnan.nextLine();
+//        deistvie = scnan.nextLine();
+//
+//        switch (deistvie){
+//            case "+":
+//                res = num1 + num2;
+//                System.out.println("Результат: " + res);
+//                break;
+//            case "*":
+//                res = num1 * num2;
+//                System.out.println("Результат: " + res);
+//                break;
+//            case "/":
+//                if (num2 == 0)
+//                    System.out.println("На ноль делить нельзя!");
+//                else{
+//                res = num1 / num2;
+//                System.out.println("Результат: " + res);
+//                }
+//                break;
+//            case "-":
+//                res = num1 - num2;
+//                System.out.println("Результат: " + res);
+//                break;
+//            default:
+//                System.out.println("Вы ввели что-то не то!");
+//        }
+//    }
+//}
+
+
+
+
+
+
+
+//public class Tests {
+//    public static void main(String[] args) {
+//        // Циклы
+//         for(float i = 100; i >= 10; i /=2){
+//             System.out.println("Element: " + i);
+//         }
+//    }
+//}
+
+
+
+
+//public class Tests {
+//    public static void main(String[] args) {
+//        Scanner scan = new Scanner(System.in);
+//        int pin = 1234;
+//        int attempt = 0;
+//        System.out.println("Введите пинкод: ");
+//        int usercode = scan.nextInt();
+//        if (usercode == pin) {
+//            System.out.println("Ваш пинкод верен!");
+//
+//        }else{
+//           do {
+//               for (attempt = 3; attempt >= 0; attempt--) {
+//                   System.out.println("Вы ввели неверный код у вас осталось " + attempt + " попыток");
+//               }
+//           }   while (usercode == pin) ;
+//
+//        }
+//    }
+//}
+
+
+
+
+
+
+
+
+//public  class Tests {
+//    public static void main(String[] args) {
+//        //Циклы
+//
+//        for (int i = 30; i >= 0; i--){
+//        System.out.println("Patron: " + i);
+//        if (i == 20) {
+//            System.out.println("Ostalosj 20 patronov");
+//        }if (i == 10) {
+//                System.out.println("Ostalosj 10 patronov");
+//            }if    (i == 0){
+//                    System.out.println("Net patronov perezarjadite oruzie");
+//             }
+//        }
+//    }
+//}
+
+
+
+
+
+//public class Tests {
+//    public static void main(String[] args) {
+//        int i = 0;
+//        while (i < 10){
+//            System.out.println("element " + i);
+//            i++;
+//        }
+//    }
+//}
+
+
+
+
+//public class Tests {
+//    public static void main(String[] args) {
+//        //Massivq
+//
+//        int[] nums = new int[5];
+//        nums[0] = 45;
+//        nums[1] = 32;
+//        nums[2] = 1;
+//        nums[3] = 9;
+//        nums[4] = 3;
+//        int res = nums[2] + nums[3];
+//        System.out.println(res);
+//
+//        float[] nums2 = new float[] {5.0f, 6.45f, 89.984f};
+//        System.out.println(nums2[1]);
+//
+//        for(int i = 0; i < nums2.length; i++ ){
+//            System.out.println( "Element: " + nums2[i]);
+//        }
+//    }
+//}
+
+
+
+//public class Tests {
+//    public static void main(String[] args) {
+//        // Massivq
+//
+//        int[] arr = new int[4];
+//        Scanner scanner = new Scanner(System.in);
+//        for( int i = 0; i < arr.length; i++){
+//            System.out.println("Vvedite 4islo; ");
+//            int value = scanner.nextInt();
+//            arr[i] = value;
+//        }
+//
+//        int min = arr[0];
+//        for(int i = 0; i < arr.length; i++) {
+//            if (arr[i] < min)
+//                min = arr[i];
+//            System.out.println("Minimal: " + min);
+//        }
+//    }
+//}
+
+
+
+
+
+
 public class Tests {
     public static void main(String[] args) {
-        Scanner scnan = new Scanner(System.in);
-        System.out.println("Введите первое число: ");
-        int num1 = scnan.nextInt();
-        System.out.println("Введите второе число: ");
-        int num2 = scnan.nextInt();
-        int res;
-        System.out.println("Введите действие: ");
-        String deistvie = scnan.nextLine();
-        deistvie = scnan.nextLine();
+        // OOP klasq i objektq
 
-        switch (deistvie){
-            case "+":
-                res = num1 + num2;
-                System.out.println("Результат: " + res);
-                break;
-            case "*":
-                res = num1 * num2;
-                System.out.println("Результат: " + res);
-                break;
-            case "/":
-                if (num2 == 0)
-                    System.out.println("На ноль делить нельзя!");
-                else{
-                res = num1 / num2;
-                System.out.println("Результат: " + res);
-                }
-                break;
-            case "-":
-                res = num1 - num2;
-                System.out.println("Результат: " + res);
+        Transport bmw = new Transport();
+        bmw.speed = 250.5f;
+        bmw.weight = 2500;
+        bmw.color = "white";
+        bmw.coordinate = new byte[] {0, 0, 0};
 
-                break;
-            default:
-                System.out.println("Вы ввели что-то не то!");
-        }
+        Transport truck = new Transport();
+        truck.speed = 140.5f;
+        truck.weight = 5600;
+        truck.color = "red";
+        truck.coordinate = new byte[] {100, 0, 100};
+
+        System.out.print("Truck speed: " + truck.speed + ". BMW speed: " + bmw.speed);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/Transport.java
@@ -0,0 +1,7 @@
+public class Transport {
+
+    public float speed;
+    public int weight;
+    public String color;
+    public byte[] coordinate;
+}
